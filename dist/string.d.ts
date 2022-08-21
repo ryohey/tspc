91f7a67f2cef33e5749874abafb38de6659b942b
ryohey <info@codingcafe.jp>
2022-08-21 21:27:15 +0900

    Remove dist dir

--- dist/string.d.ts
+++
@@ -1,3 +0,0 @@
-import { Parser } from "./core";
-export declare const token: <T extends string>(word: T) => Parser<string, T>;
-export declare const regexp: (reg: RegExp) => Parser<string, string>;
